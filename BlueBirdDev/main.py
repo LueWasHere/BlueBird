8dea320e5f2c0c4dbf38b261d8f501cbcfa31ed4
LueWasHere <82827429+LueWasHere@users.noreply.github.com>
2022-03-15 10:29:59 -0600

    Update main.py

--- BlueBirdDev/main.py
+++ BlueBirdDev/main.py
@@ -1,4 +1,19 @@
 import lexer
+from colorama import Fore
 
-inputs = input()
-lexer.from_input(inputs, True)
+dev = False
+
+tokens = []
+
+while True:
+    print(Fore.WHITE, end='')
+    inputs = input()
+    tokens = lexer.from_input(inputs, dev)
+    print(Fore.WHITE, end='')
+    #print(tokens) # only used if I need to see the tokens without activating devMode
+    if tokens == ['<dev>', '<PREN_LEFT>', '<PREN_RIGHT>']:
+        if dev:
+            dev = False
+        else:
+            dev = True
+        print(dev)
