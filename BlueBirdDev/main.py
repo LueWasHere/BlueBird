9f3255245dbc704f1732f725042e07c59b6a5703
LueWasHere <82827429+LueWasHere@users.noreply.github.com>
2022-03-17 11:06:11 -0600

    Update main.py

--- BlueBirdDev/main.py
+++ BlueBirdDev/main.py
@@ -11,10 +11,16 @@ while True:
     inputs = input()
     tokens = lexer.from_input(inputs, dev)
     print(Fore.WHITE, end='')
+    #print(tokens)
+    if dev:
+        print(Fore.GREEN + '[DEV] ' + Fore.LIGHTYELLOW_EX + 'Begining super token cunstruction with prim token list')
+    print(Fore.WHITE, end='')
     tokens = tokenLEX.globalLex(tokens, dev)
+    print(Fore.WHITE, end='')
     #print(tokens) # only used if I need to see the tokens without activating devMode
     if dev:
-        print('SUP TOKENS: ' + str(tokens))
+        print(Fore.GREEN + '[DEV] ' + Fore.LIGHTYELLOW_EX + 'SUP TOKENS: ' + str(tokens))
+    print(Fore.WHITE, end='')
     if tokens == ['<CALL>', '<dev>']:
         if dev:
             dev = False
